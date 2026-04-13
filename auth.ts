269b6c6a4249d7bedc9fe20d59099ce6f207fc65
nic-ball <nicholasball147@gmail.com>
2026-04-13 06:37:22 +0100

    Add trustHost to Auth.js

--- auth.ts
+++ auth.ts
@@ -2,6 +2,7 @@ import NextAuth from 'next-auth'
 import credentials from 'next-auth/providers/credentials'
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  trustHost: true,
   providers: [
     credentials({
       credentials: {
